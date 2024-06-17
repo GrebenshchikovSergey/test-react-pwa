478d179e77697d8cb1185e1d7b0f1b047f727990
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-17 11:55:31 +0700

    add notification

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const MainPage = () => {
-	return <div>MainPage</div>;
+	const sendPush = async () => {
+		const perm = await Notification.requestPermission();
+		if (perm !== "granted") {
+			return;
+		}
+		new Notification("Hi everyone", {
+			body: "test test",
+		});
+	};
+	return (
+		<div>
+			<button onClick={sendPush}>sendPush</button>
+		</div>
+	);
 };
 
 export default MainPage;
