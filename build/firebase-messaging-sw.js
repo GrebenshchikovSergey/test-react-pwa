8a26958e4be2b925a60e622e127b55ea7ae0c877
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-21 11:08:50 +0700

    add fcm

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+	apiKey: "AIzaSyAIEo2Tce5G1jAM-9DR4Q8jLsnmXN_pWgQ",
+	authDomain: "ksk-softorium-brand.firebaseapp.com",
+	projectId: "ksk-softorium-brand",
+	storageBucket: "ksk-softorium-brand.appspot.com",
+	messagingSenderId: "837625796017",
+	appId: "1:837625796017:web:3d5f6ce59fdd4f199fd990",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+	console.log("Received background message ", payload);
+
+	const notificationTitle = payload.notification.title;
+	const notificationOptions = {
+		body: payload.notification.body,
+	};
+
+	self.registration.showNotification(notificationTitle, notificationOptions);
+});
