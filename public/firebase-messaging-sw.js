c1aed8c10ed1ff44f2e650b022c9afec5b7b6456
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-21 15:32:35 +0700

    client list

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -33,6 +33,7 @@ self.addEventListener("notificationclick", function (event) {
 	event.notification.close();
 	event.waitUntil(
 		clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+			console.log("clientlist: ", clientList);
 			if (clientList.length > 0) {
 				let client = clientList[0];
 				for (let i = 0; i < clientList.length; i++) {
