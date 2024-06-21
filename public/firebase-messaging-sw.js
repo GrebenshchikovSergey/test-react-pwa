23c6a120596267e35752ed40eb2e43ae8b2565cf
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-21 16:53:57 +0700

    'fix'

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,7 +30,7 @@ messaging.onBackgroundMessage(function (payload) {
 self.addEventListener("notificationclick", function (event) {
 	console.log("Notification click Received.");
 	console.log("self.clients", clients.matchAll({ type: "window", includeUncontrolled: true }));
-	self.clients.matchAll().then((clients) => {
+	clients.matchAll({ type: "window", includeUncontrolled: true }).then((clients) => {
 		console.log("clients", clients);
 		clients.forEach((client) => {
 			client.postMessage({ msg: "Hello from SW" });
