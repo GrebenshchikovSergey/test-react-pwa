63ec033db300d00df731e267eeeeeeaf26197d43
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-25 09:48:10 +0700

    'fix'

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -55,3 +55,11 @@ self.addEventListener("notificationclick", function (event) {
 		})
 	);
 });
+
+self.addEventListener("install", function (event) {
+	event.waitUntil(self.skipWaiting()); // Activate worker immediately
+});
+
+self.addEventListener("activate", function (event) {
+	event.waitUntil(self.clients.claim()); // Become available to all pages
+});
