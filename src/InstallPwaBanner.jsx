215a08a415a1a4f8ce68a65c6ec56498a5791d7b
Grebenshchikov Sergey <Grebenshchikov@softorium.pro>
2024-06-25 18:27:36 +0700

    'fix'

--- src/InstallPwaBanner.jsx
+++ src/InstallPwaBanner.jsx
@@ -40,7 +40,7 @@ const InstallPwaBanner = ({ token }) => {
 		<div
 			style={{
 				position: "fixed",
-				bottom: "10px",
+				top: "10px",
 				left: "50%",
 				transform: "translateX(-50%)",
 				zIndex: 1000,
